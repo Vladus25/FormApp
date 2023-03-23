eeb96033078c4883b21a96bbe9c7ab261eaae789
Vladyslav Okopnyy <harmfulcati@gmail.com>
2023-03-23 17:49:58 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormApp';
+
+  constructor(private fb:FormBuilder){
+
+  }
+  myform=this.fb.group({
+    name:['',Validators.required],
+    indirizzo:this.fb.group({
+      via:['',Validators.required]
+    })
+  })
+
+  onSubmit(){
+    console.log(this.myform.value);
+  };
+
+  // nome!: string;
+  // cognome!: string;
+
+  // invio(form: any){
+  //   console.log(JSON.stringify(form));
+  //   console.log(form.nome);
+  //   console.log(form.cognome);
+  // }
+
+  // model = new User()
+
 }
+
+// export class User{
+//   nome!: string;
+//   cognome!: string;
+// }
